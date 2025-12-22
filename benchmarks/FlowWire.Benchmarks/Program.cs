37458921066f3c2c6c17fd89979104dab32fc800
Mathias Døhl <fckmathias@hotmail.com>
2025-12-22 14:43:40 +0100

    Add FlowWire.Benchmarks project with BenchmarkDotNet
    Introduced a new benchmarks project under benchmarks/FlowWire.Benchmarks using BenchmarkDotNet. Updated the solution file to include the new project, which references FlowWire.Abstractions and provides a Program.cs entry point for running benchmarks.

---
+++ benchmarks/FlowWire.Benchmarks/Program.cs
@@ -0,0 +1,8 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace FlowWire.Benchmarks;
+
+public class Program
+{
+    static void Main(string[] args) => BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+}
